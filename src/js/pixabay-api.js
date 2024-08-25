4ce9388fb751a59226091e60b19ea5ad37c0f23a
Anastasia <tildatopdev@gmail.com>
2024-08-25 16:07:07 +0300

    add styles.css

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,4 +1,5 @@
 const BASE_URL = 'https://pixabay.com/api/?';
+
 export const fetchPhotos = searchedValue => {
   const urlParams = new URLSearchParams({
     key: '45539852-e7385dbf9677b23660ec365b6',
@@ -7,10 +8,12 @@ export const fetchPhotos = searchedValue => {
     orientation: 'horizontal',
     safesearch: true,
   });
+
   return fetch(`${BASE_URL}${urlParams}`).then(response => {
     if (!response.ok) {
       throw new Error(response.status);
     }
+
     return response.json();
   });
 };
--- src/main.js
+++ src/main.js
@@ -1,11 +1,23 @@
 import { createGalleryCardTemplate } from './js/render-functions';
 import { fetchPhotos } from './js/pixabay-api';
 import iziToast from 'izitoast';
+import SimpleLightbox from 'simplelightbox';
+
 const searchFormEl = document.querySelector('.js-search-form');
 const galleryEl = document.querySelector('.js-gallery');
+const loaderEl = document.querySelector('.loader');
+const simpleLightbox = new SimpleLightbox('.js-gallery a', {
+  captionDelay: 250,
+  captionPosition: 'bottom',
+  captionsData: 'alt',
+  overlayOpacity: 1,
+});
+
 const onSearchFormSubmit = event => {
   event.preventDefault();
   const searchedValue = searchFormEl.elements.user_query.value.trim();
+  loaderEl.classList.remove('is-hidden');
+
   fetchPhotos(searchedValue)
     .then(data => {
       if (data.hits.length === 0) {
@@ -16,12 +28,15 @@ const onSearchFormSubmit = event => {
         });
         galleryEl.innerHTML = '';
         searchFormEl.reset();
+
         return;
       }
       const galleryCardsTemplate = data.hits
         .map(imgDetails => createGalleryCardTemplate(imgDetails))
         .join('');
       galleryEl.innerHTML = galleryCardsTemplate;
+      simpleLightbox.refresh();
+      loaderEl.classList.add('is-hidden');
     })
     .catch(err => {
       console.log(err);
