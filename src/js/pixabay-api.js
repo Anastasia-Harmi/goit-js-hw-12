a984ee51a7a2c5936da53ebe226a936c71347f4f
Anastasia <tildatopdev@gmail.com>
2024-08-31 15:51:48 +0300

    fix file pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,19 +1,18 @@
-const BASE_URL = 'https://pixabay.com/api/?';
+import axios from 'axios';
+axios.defaults.baseURL = 'https://pixabay.com/api/'; //звертаюсь до власт defaults.baseURL і тепер з доп бібл axios  до get запиту автоматично дод.цей шлях. а вказуємо в запиті тепер лише русурси
 
-export const fetchPhotos = searchedValue => {
-  const urlParams = new URLSearchParams({
-    key: '45539852-e7385dbf9677b23660ec365b6',
-    q: searchedValue,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-  });
-
-  return fetch(`${BASE_URL}${urlParams}`).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-
-    return response.json();
-  });
+export const fetchPhotos = (searchedValue, page) => {
+  //передаємо знач інпута та номер стр для показу фото
+  const axiosOptions = {
+    params: {
+      key: '45539852-e7385dbf9677b23660ec365b6',
+      q: searchedValue,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      page: page,
+      per_page: 15,
+    },
+  };
+  return axios.get('', axiosOptions); // лапки вказують на те, що тут використ базовий url
 };
